5b4235c42ed1114f018a3e718c3ddaee779ba953
Max1991 <max.breton@hotmail.fr>
2014-08-29 12:34:09 +0200

    First Commit

---
+++ MvcApplication1/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        [HttpGet]
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                InsertContact(model.Name, model.Email, model.Comments);
+                TempData["notice"] = "Your form was submitted.";
+                //Envoi à nouveau les informations
+                //return view();
+
+                //Ne peut réenvoyer les données avec un refresh
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+
+        private void InsertContact(string name, string email, string comments)
+        {
+
+        }
+    }
+}
---
+++ MvcApplication1/Models/ContactModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class ContactModel
+    {
+        //Champs obligatoires
+        [Required(ErrorMessage="Required")] //Affiche Requi seulement
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@".*@.*", ErrorMessage = "Must be a valid email")]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Comments { get; set; }
+    }
+}
